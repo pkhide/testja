2ce654013d0f7df118eb886ff948790941958b39
renovatio_ <desmorider@outlook.com>
2018-10-21 17:37:51 +0700

    test connection DB

--- app/src/main/java/com/example/prin/testja/MainActivity.java
+++ app/src/main/java/com/example/prin/testja/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.prin.testja;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private EditText getTxt;
     private Button btnOK;
-
     private String txtName;
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +45,63 @@ public class MainActivity extends AppCompatActivity {
 
                 // display text in alert dialog
                 Toast.makeText(MainActivity.this, txtName, Toast.LENGTH_SHORT).show();
-
             }
         });
+
+        listView = (ListView) findViewById(R.id.lisstView);
+        downloadJSOn("http://64.52.86.78/api/stock_service.php");
+    }
+
+    private void downloadJSOn(final String urlWebService){
+
+        class DownloadJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        DownloadJSON getJSON = new DownloadJSON();
+        getJSON.execute();
+    }
+
+    private  void loadIntoListView(String json) throws JSONException{
+        JSONArray jsonArray = new JSONArray(json);
+        String[] stocks = new String[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            stocks[i] = obj.getString("name") + " " + obj.getString("price");
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stocks);
+        listView.setAdapter(arrayAdapter);
     }
 }
