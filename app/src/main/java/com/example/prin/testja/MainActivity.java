f1a227e83f6b239b3408a9ce0c8e485f3698bf45
prin <desmorider@outlook.com>
2018-10-21 14:31:48 +0700

    add plantext and display text as alert dialog.

--- app/src/main/java/com/example/prin/testja/MainActivity.java
+++ app/src/main/java/com/example/prin/testja/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.prin.testja;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText getTxt;
+    private Button btnOK;
+
+    private String txtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bind widget
+        getTxt = findViewById(R.id.txtName);
+        btnOK = findViewById(R.id.btnOK);
+
+        // create event listener on button OK
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtName = getTxt.getText().toString();
+
+                // display text in alert dialog
+                Toast.makeText(MainActivity.this, txtName, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
